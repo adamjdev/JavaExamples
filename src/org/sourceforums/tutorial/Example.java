5e8f8e836be2bce8102e5cbeac28b0ec8f6bf43b
TekkitCommando <admin@sourceforums.org>
2014-12-07 21:46:51 -0600

    First Commit

---
+++ src/org/sourceforums/tutorial/Example.java
@@ -0,0 +1,13 @@
+package org.sourceforums.tutorial;
+
+public class Example {
+
+	public static void main(String[] args) {		
+		
+		int cows = 9;
+		
+		System.out.println("Hi you have " + cows + " cows");
+
+	}
+
+}
