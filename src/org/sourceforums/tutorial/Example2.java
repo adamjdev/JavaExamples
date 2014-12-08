ef67fa339054948454245544e495319ebef3e36d
TekkitCommando <admin@sourceforums.org>
2014-12-07 21:53:29 -0600

    Second Commit

--- src/org/sourceforums/tutorial/Example2.java
+++ src/org/sourceforums/tutorial/Example2.java
@@ -4,20 +4,18 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		
-		int cupcakes = 21;
+		int cupcakes = 20;
 		int cupcakeGoal = 20;
 		
 		if(cupcakes == cupcakeGoal) {
 			System.out.println("We have met our goal for this year");
 		} else if(cupcakes > cupcakeGoal) {
-			System.out.println("We have made more then enough cupcakes");
-		
+			System.out.println("We have made more then enough cupcakes");		
 		} else {
 			System.out.println("We have not met our goal :-(");
 		}
 		
-
-		}
-
 	}
+
+}
 
