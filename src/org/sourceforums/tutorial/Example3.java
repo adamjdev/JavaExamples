81de405d44201529ddec6e671566366acc71ab1e
TekkitCommando <admin@sourceforums.org>
2014-12-10 17:12:36 -0600

    Added Example3

---
+++ src/org/sourceforums/tutorial/Example3.java
@@ -0,0 +1,16 @@
+package org.sourceforums.tutorial;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		int count = 0, amount = 10;
+		
+		while(count < amount) {
+			System.out.println("Count: " + count);
+			
+			count++;
+		}
+
+	}
+
+}
